c1b04c9278bb028738a08ceb250c0836140240c8
Timothy Jesionowski <timothy.n.jesionowski@gmail.com>
2023-08-14 04:48:59 -0400

    Got cubies moving.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from manim import *
 from manim_rubikscube import *
 
@@ -6,8 +8,8 @@ class AllTogetherExample(ThreeDScene):
         # Change the cube from default colors
         cube = RubiksCube(colors=[WHITE, ORANGE, DARK_BLUE, YELLOW, PINK, "#00FF00"]).scale(0.6)
 
-        self.move_camera(phi=50*DEGREES, theta=160*DEGREES)
-        self.renderer.camera.frame_center = cube.get_center()
+        self.move_camera(phi=50*DEGREES, theta=30*DEGREES)
+        #self.renderer.camera.frame_center = cube.get_center()
 
         # Set the state of the cube
         state = "BBFBUBUDFDDUURDDURLLLDFRBFRLLFFDLUFBDUBBLFFUDLRRRBLURR"
@@ -17,11 +19,19 @@ class AllTogetherExample(ThreeDScene):
         self.wait()
 
         # Loop through results of the kociemba algorithm
-        for m in cube.solve_by_kociemba(state):
-            # Execute the move
-            self.play(CubeMove(cube, m), run_time=1.5)
+        # for m in cube.solve_by_kociemba(state):
+        #     # Execute the move
+        #     self.play(CubeMove(cube, m), run_time=1.5)
 
         # Show the final product
         self.play(
-            Rotating(cube, radians=2*PI, run_time=2)
+            AnimationGroup(
+                # Succession(*[cubie.faces.animate.indicate() for cubie
+                #              in np.nditer(cube.cubies, flags=["refs_ok"],
+                #                           op_flags=["readwrite"])],
+                #            lag_ratio=0.5)
+                cube.cubies[0,0,0].animate.shift(DOWN),
+                cube.cubies[1,1,1].animate.shift(DOWN),
+                cube.cubies[2,2,2].animate.shift(DOWN)
+            )
         )
